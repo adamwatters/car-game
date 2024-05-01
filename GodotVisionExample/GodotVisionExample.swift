7f46ec9f786206d235629742a241365fa7545f1e
Adam Watters <adamwatters11@gmail.com>
2024-04-30 23:02:26 -0700

    rough draft track

--- GodotVisionExample/GodotVisionExample.swift
+++ GodotVisionExample/GodotVisionExample.swift
@@ -6,7 +6,7 @@ import MultipeerConnectivity
 // visionOS will silently clamp to volume size if set above or below limits
 // max for all dimensions is 1.98
 // min for all dimensions is 0.24
-let VOLUME_SIZE = simd_double3(1.5, 1, 1.5)
+let VOLUME_SIZE = simd_double3(1.6, 0.941, 1.6)
 
 @main
 struct GodotVisionExample: App {
